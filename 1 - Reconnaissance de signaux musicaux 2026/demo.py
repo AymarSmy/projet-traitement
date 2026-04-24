57267f2e65798d3efb7959d7125e210c29900979
Maxime Cuvelier <maxime.cuvelier@etu.minesparis.psl.eu>
2026-04-24 19:39:23 +0200

    trouver le 602

--- 1 - Reconnaissance de signaux musicaux 2026/demo.py
+++ 1 - Reconnaissance de signaux musicaux 2026/demo.py
@@ -48,11 +48,10 @@ if __name__ == '__main__':
     tmin = int(tstart*fs)
     duration = int(10*fs)
 
-    # 4: Use the encoder to extract a signature from the extract
     encoder.process(fs, s[tmin:tmin + duration])
     hashes = encoder.hashes
 
-    # 5: Comparaison avec la base de données
+    # Comparaison avec la base de données
     scores = []
     names = []
 
@@ -63,7 +62,7 @@ if __name__ == '__main__':
         scores.append(score)
         names.append(entry['song'])
 
-    # 6: Résultat final
+    # Résultat final
     best_idx = np.argmax(scores)
     print("\n" + "="*30)
     print(f"RÉSULTAT : {names[best_idx]}")
@@ -83,4 +82,64 @@ if __name__ == '__main__':
 
     #Affichage histogramme pour une autre chanson (question 6)
     no_matcher.display_histogram()
+
+
+if __name__ == '__main__':
+
+    # 1: Chargement de la base de données
+    with open('songs.pickle', 'rb') as handle:
+        database = pickle.load(handle)
+
+    # 2: Encoder
+    nperseg=128
+    noverlap=32
+    min_distance=25
+    time_window=1.
+    freq_window=1500
+    encoder = Encoding(nperseg=nperseg, noverlap=noverlap, 
+      min_distance=min_distance,
+      time_window=time_window, 
+      freq_window=freq_window)
+
+    # 3: Randomly get an extract from one of the songs of the database
+    # On ouvre le morceau secret
+    filename = './secret_sample.wav'
+
+    fs, s = read(filename)
+    #toujours pour régler le problème de stéréo
+    if len(s.shape) > 1:
+        s = s[:, 0]
+    #sinon on a des problèmes d'extrait trop courts
+    total_duration = len(s) / fs
+    segment_length = 10
+    if total_duration > segment_length:
+        tstart = np.random.uniform(0, total_duration - segment_length)
+    else:
+        tstart = 0
+        segment_length = total_duration
+    tmin = int(tstart*fs)
+    duration = int(10*fs)
+
+
+    encoder.process(fs, s[tmin:tmin + duration])
+    hashes = encoder.hashes
+
+    #Comparaison avec la base de données
+    scores = []
+    names = []
+
+
+    for entry in database:
+        matcher = Matching(hashes, entry['hashcodes'])
+        score = matcher.get_score()
+        scores.append(score)
+        names.append(entry['song'])
+
+    # Résultat final
+    best_idx = np.argmax(scores)
+    print("\n" + "="*30)
+    print(f"RÉSULTAT : {names[best_idx]}")
+    print(f"SCORE : {scores[best_idx]}")
+    print("="*30)
+
 
